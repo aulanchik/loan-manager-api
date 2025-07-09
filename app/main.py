08a1ff1d03c5fbb2e9786cd967807155151ea709
Artyom Ulanchik <artyom.u@outlook.com>
2025-07-09 13:47:56 +0300

    prepping for render deployment

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI, Request, HTTPException
 from app import storage, schemas, country_service, rate_limiter
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.post("/apply-loan")
 async def apply_loan(request: Request, application: schemas.LoanApplication):
-    ip = request.client.host
+    ip = request.client.host or "127.0.0.1"
     country_code = await country_service.get_country_code(ip)
-    
+
     if storage.is_blacklisted(application.personal_id):
         raise HTTPException(status_code=400, detail="Blacklisted personal ID")
-    
+
     if rate_limiter.is_rate_limited(country_code):
         raise HTTPException(status_code=429, detail="Rate limit exceeded for country")
-    
+
     storage.store_loan(application, country_code)
     return {"status": "approved", "country": country_code}
 
