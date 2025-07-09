f3df351c9ef1c8693d50a3cddda739362d97d5f0
Artyom Ulanchik <artyom.u@outlook.com>
2025-07-09 13:16:05 +0300

    set rate limitation on requests

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
-from fastapi import FastAPI, HTTPException
-from app import schemas, storage
+from fastapi import FastAPI, Request, HTTPException
+from app import storage, schemas, country_service, rate_limiter
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.post("/apply-loan")
-def apply_loan(application: schemas.LoanApplication):
-    country_code = "GB"  # hardcoded for now
 
+@app.post("/apply-loan")
+async def apply_loan(request: Request, application: schemas.LoanApplication):
+    ip = request.client.host
+    country_code = await country_service.get_country_code(ip)
+    
     if storage.is_blacklisted(application.personal_id):
         raise HTTPException(status_code=400, detail="Blacklisted personal ID")
-
+    
+    if rate_limiter.is_rate_limited(country_code):
+        raise HTTPException(status_code=429, detail="Rate limit exceeded for country")
+    
     storage.store_loan(application, country_code)
     return {"status": "approved", "country": country_code}
+
 
 @app.get("/loans")
 def list_loans():
     return storage.get_all_loans()
+
 
 @app.get("/loans/{personal_id}")
 def list_loans_by_user(personal_id: str):
